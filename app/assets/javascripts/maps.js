7e3a83279df1952c47cfb977cfeb27d6b60c1ab9
Michael Jasinski <email": null>
2016-01-29 14:46:12 -0600

    Add search function for keyword location

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -33,36 +33,70 @@
   // }
 
 
+  var map;
+  var service;
+  var infowindow;
+
   function initMap() {
-    var myLatLng = {lat: 41.885311, lng: -87.62850019999999};
+    var chicago = new google.maps.LatLng(41.885311, -87.62850019999999);
 
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
-      center: myLatLng
-    });
-    var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
+      center: chicago
     });
 
-    google.maps.event.addListener(map, 'click', function(event) {
+    var request = {
+      location: chicago,
+      radius: '500',
+      keyword: ['pizza']
+    };
 
-       console.log(event.latLng.lat());
-       console.log(event.latLng.lng());
-       placeMarker(event.latLng, map);
-    });
-    google.maps.event.addListener(map, 'rightclick', function(e) {
-
-      placeMarker(event.latLng, map);
-  });
-
-    function placeMarker(position, map) {
-      var marker = new google.maps.Marker({
-        position: position,
-        map: map
-      });
-
-    }
-
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
   }
+
+
+  function callback(results, status){
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for(var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+
+
+
+  //   var marker = new google.maps.Marker({
+  //   position: chicago,
+  //   map: map,
+  //   title: 'Hello World!'
+  //   });
+
+  //   google.maps.event.addListener(map, 'click', function(event) {
+
+  //      console.log(event.latLng.lat());
+  //      console.log(event.latLng.lng());
+  //      placeMarker(event.latLng, map);
+  //   });
+
+  //   function placeMarker(position, map) {
+  //     var marker = new google.maps.Marker({
+  //       position: position,
+  //       map: map
+  //     });
+  //   }
+
+  // }
