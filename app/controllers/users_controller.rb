1a6d16cfbc1d8913ade123b01976f0b2728debcd
wgranger <twgranger@gmail.com>
2016-01-29 14:20:02 -0600

    Begin BCrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to @user
+    else
+      redirect_to :back
+    end
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :city, :state, :email)
   end
 
 end
