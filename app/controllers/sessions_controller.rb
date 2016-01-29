e02edd80f1fdd880b597450cbbb805167e9d0215
wgranger <twgranger@gmail.com>
2016-01-29 15:15:00 -0600

    Finish Auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.user_id
+      session[:user_id] = user.id
       redirect_to user
     else
       redirect_to :back
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,19 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect to @user
+      redirect_to @user
     else
       redirect_to :back
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :username, :city, :state, :email)
+    params.require(:user).permit(:first_name, :last_name, :username, :city, :state, :email, :password)
   end
 
 end
