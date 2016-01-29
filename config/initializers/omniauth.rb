e4f9e7a644cca6ec39c2030324367a5f24a5965a
J Shin <jjshin85@gmail.com>
2016-01-29 10:44:37 -0600

    fix typo in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["GMAPS"], ENV["GSECRETggem"]
+  provider :google_oauth2, ENV["GMAPS"], ENV["GSECRET"]
 end
