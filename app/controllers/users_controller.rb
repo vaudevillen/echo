6daec2eda13753ca78fabe10ab678d083342f8ee
Michael Jasinski <email": null>
2016-01-30 21:52:03 -0600

    Add partial for edit settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      redirect_to 'edit_user_path'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :username, :city, :state, :email, :password)
