7761053223d309ac98fad90bfaf260bb94f87c7f
J Shin <jjshin85@gmail.com>
2016-01-31 21:21:58 -0600

    modify friends sidebar

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  $("#friends_sidebar").on("click")
+  $("#friends_sidebar").toggle();
+  $("#friends_toggle").on("click", function(event){
+    $("#friends_sidebar").toggle();
+    if($("#friends_toggle").text() == "Show Friends Bar")
+    {
+      $("#friends_toggle").text("Hide Friends Bar");
+    }
+    else
+    {
+      $("#friends_toggle").text("Show Friends Bar");
+    }
+  });
 });
