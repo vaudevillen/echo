37e7524d89a093d05a70e8360d25328387e4a9eb
Michael Jasinski & Will Granger <pair+michael-jas.wgranger@devbootcamp.com>
2016-02-01 11:22:51 -0600

    Fix react river function, stream updating

--- app/assets/javascripts/components/river.js.jsx
+++ app/assets/javascripts/components/river.js.jsx
@@ -1,23 +1,40 @@
-var Friends = React.createClass({
-  render() {
-    var createItem = (friend) => <li>{friend.first_name}</li>;
-
-    return <ul>{this.props.friends.map(createItem)}</ul>;
+var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function() {
+    this.intervals.forEach(clearInterval);
   }
-});
+};
 
 var Comment = React.createClass({
-  render() {
-    // var createItem = ({comment}) => <Comment comment={comment} />;
 
-    return <div>{this.props.comments}</div>;
+  mixins: [SetIntervalMixin],
+  getInitialState: function(){
+    console.log(this.props.pins);
+
+    return {seconds: 0,  pins: this.props.pins};
+  },
+
+  componentDidMount: function(){
+    this.setInterval(this.comment, 5000);
+  },
+
+  comment: function(){
+    this.props.pins.push({comment: "willy nilly"})
+    this.setState({pins: this.props.pins});
+  },
+
+  render() {
+    console.log(this.state.pins);
+     return (<div>{this.state.pins.map(function (key, value){
+        return <div key={key.id}>
+         Name: {key.comment}
+        </div>;
+      })}
+    </div>)
   }
 });
-
-// var River = React.createClass({
-//   render() {
-//     var createItem = ({comment}) => <Comment comment={comment} />;
-
-//     return <div>{this.props.comments.map(createItem)}</div>;
-//   }
-// });
