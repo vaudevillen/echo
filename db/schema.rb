1575dfdb570e4760a53f4ff8bf78459b2cf11e1c
Ryan Zell <ryanzell36@gmail.com>
2016-01-31 21:58:10 -0600

    Add profile pics with paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129172107) do
+ActiveRecord::Schema.define(version: 20160201032253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,8 +57,12 @@ ActiveRecord::Schema.define(version: 20160129172107) do
     t.string   "state"
     t.string   "password_digest"
     t.string   "token"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
