6fa6233405799e2088b81f265ef1430a402827d0
Ryan Zell <ryanzell36@gmail.com>
2016-02-03 15:11:08 -0600

    S3 Asset Pipeline

---
+++ public/assets/components/river-870967229407d5fc956a675c6ff42d60d5fe30fbe640124601eb4fac979de667.jsx
@@ -0,0 +1,46 @@
+var SetIntervalMixin = {
+  componentWillMount: function() {
+    this.intervals = [];
+  },
+  setInterval: function() {
+    this.intervals.push(setInterval.apply(null, arguments));
+  },
+  componentWillUnmount: function() {
+    this.intervals.forEach(clearInterval);
+  }
+};
+
+
+// React Comp called in _navbar
+var Comment = React.createClass({
+
+  mixins: [SetIntervalMixin],
+  getInitialState: function(){
+    return {seconds: 0,  pins: this.props.pins};
+  },
+
+  componentDidMount: function(){
+    this.setInterval(this.comment, 5000);
+  },
+
+  comment: function(){
+    $.ajax({
+      url: "/maps",
+      dataType: 'json',
+      type: 'GET',
+      cache: false,
+      success: function(data){
+        this.setState({pins: data});
+      }.bind(this)
+    });
+
+  },
+  render: function() {
+     return (<div>{this.state.pins.map(function (key, value){
+        return <div className="river_div" key={key.id} data-lat={key.latitude} data-lng={key.longitude} data-user_id={key.user_id}>
+         <p className='river'>{key.username} @ {key.location} {key.address} Click here Song: {key.artist} - {key.comment}</p>
+        </div>;
+      })}
+    </div>)
+  }
+});
