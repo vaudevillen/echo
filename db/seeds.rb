a178fd3ceaaa747535e6abcae6fdf2fef047fab7
wgranger <twgranger@gmail.com>
2016-02-04 15:36:33 -0600

    Seed DB with User Info

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,8 +25,7 @@ end
       )
 end
 
-
-40.times do
+30.times do
   Pin.create(
     username: Faker::Name.first_name,
     user_id: Faker::Number.between(1, 10),
@@ -37,14 +36,22 @@ end
   )
 end
 
+will = User.create(first_name: 'Will', last_name: 'Granger', username: 'GrangerousTimes', password: 'dachshund', email: "twgranger@gmail.com", city: "Chicago", state: "IL", avatar: URI.parse('https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/8/005/036/258/01e168f.jpg'))
+# Pin.create(user_id: will.id, song_id: "35w541m4SS6eqKFHmFNu47", comment: "My first show in Chicago!", address: "2100 W Belmont Ave, Chicago, IL 60618, USA", username: "Will Granger", created_at: "2016-02-04 20:46:29", updated_at: "2016-02-04 20:46:29", song_artist: "Nobunny", song_title: "Gone For Good")
+# Pin.create(user_id: will.id, song_id: "7ah8dRCjJFGCWstleG0PpD", comment: "KKill a Punk for Rock and Roll. This was the first line I noticed on the cover of 'Popular Favorites,' and each time I listen to the record reminds me of the four hour trip from Birmingham to Memphis to see their reunion in 2008. We wouldn't make it back to Birmingham until after 4am and a close call at an ATM when we almost got robbed. The Oblivians made the trip up north to play a New Year's Eve show, and I was able to walk home at midnight without any late-night trips to an ATM.", address: "1035 N Western Ave, Chicago, IL 60622, USA", username: "Will Granger", created_at: "2016-02-04 21:02:39", updated_at: "2016-02-04 21:02:39", song_artist: "Oblivians", song_title: "Christina")
+# Pin.create(user_id: will.id, song_id: "4FMNpPI7mkhdeFCCk6fZiE", comment: "'Borderline' is the sound of taking the metro home late at night while smiling and dancing with yourself. You can be a little drunk, and passengers can make their own assumptions about what's going on in your world.", address: "100 W Randolph St, Chicago, IL 60601, USA", username: "Will Granger", created_at: "2016-02-04 21:06:37", updated_at: "2016-02-04 21:06:37", song_artist: "Madonna", song_title: "Borderline")
+# Pin.create(user_id: will.id, song_id: "6jzbUvXYVbH6NrUS3WRMoL", comment: "Fela's records had been out of print for years until they were finally reissued by Knitting Factory. With that, the resurgence of Afro-beat came back. I was a bit hesitant about moving to Chicago on a whim in 2015, but after finding this is the world section of a local record store, I decided I'd find my place in a new city.", address: "1379 N Milwaukee Ave, Chicago, IL 60622, USA", username: "Will Granger", created_at: "2016-02-04 21:11:03", updated_at: "2016-02-04 21:11:03", song_artist: "Fela Kuti", song_title: "Water No Get Enemy")
+# Pin.create(user_id: will.id, song_id: "2mWC65WOZSBZnaacXaYoBR", comment: "One day, when money isn't so tight, I'm going to step into a bar and play 'Cowboy Song' twenty times on a jukebox. I haven't decided if I will stay or not, but I'm curious how long it would take people to notice what's happening.", address: "222 W Kinzie St, Chicago, IL 60654, USA", username: "Will Granger", created_at: "2016-02-04 21:18:03", updated_at: "2016-02-04 21:18:03", song_artist: "Thin Lizzy", song_title: "Cowboy Song")
+
 melissa = User.create(first_name: "Melissa", last_name: "Etheridge", username: "onlyone", password: "window", email: "come2window@google.com", city: "Leavenworth", state: 'KS', avatar: File.open(File.join(Rails.root, "/app/assets/images/melissa.png")))
-FriendRequest.create(recipient_id: 11, sender_id: 1, status: true)
-FriendRequest.create(recipient_id: 11, sender_id: 2, status: true)
-FriendRequest.create(recipient_id: 11, sender_id: 3, status: true)
-FriendRequest.create(recipient_id: 11, sender_id: 4, status: true)
-FriendRequest.create(recipient_id: 11, sender_id: 5, status: true)
-FriendRequest.create(recipient_id: 11, sender_id: 6, status: true)
-FriendRequest.create(recipient_id: 11, sender_id: 7, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 1, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 2, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 3, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 4, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 5, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 6, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 7, status: true)
+FriendRequest.create(recipient_id: 12, sender_id: 11, status: true)
 
 Pin.create(user_id: 1, comment: "I really like this song, Go Cards!", song_id: "Hit Song", username: "Ricky Martin")
 Pin.create(user_id: 2, comment: "This really gets me going", song_id: "Booty Dance", username: "Dan Davis")
