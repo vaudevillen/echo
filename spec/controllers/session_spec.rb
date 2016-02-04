aa5d171a64f5d1393dc5a8f6006b1f03bc8d672f
Michael Jasinski <majasinski8@gmail.com>
2016-02-04 11:07:51 -0600

    Refactor test 100% covered

--- spec/controllers/session_spec.rb
+++ spec/controllers/session_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 describe SessionsController do
   let! (:mike) { User.create!(first_name: "mike", last_name: "j", username: 'mikey', email: "m@m.com", city: "Chicago", state: "IL", password: "tort")
   }
-  let!(:user) { User.find_by(username: "mikey") }
-
+  
   describe "POST #create" do
     context 'When the users info is valid' do
       it "Creates a new session for a user" do
@@ -15,7 +14,6 @@ describe SessionsController do
     context 'When the users info is invalid' do
       it 're-renders the login page with error' do
         post(:create, username: "mikey", password: "badpassword")
-
         expect(response).to redirect_to("/login")
         expect(flash[:error]).to match("Your username and password do not match")
       end
